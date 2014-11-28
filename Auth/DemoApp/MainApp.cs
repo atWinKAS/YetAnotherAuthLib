0a87f659dc6dbee39f0297ae7d121cdd70315057
Andrii Tkach <at@winkas.dk>
2014-11-28 14:05:30 +0200

    SUT
    Added new System Under Test application where we will test out auth
    system

---
+++ Auth/DemoApp/MainApp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoApp
+{
+    public class MainApp
+    {
+        public void AllCanDoThis()
+        {
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("Any person can invoke this method.");
+            Console.WriteLine(Environment.NewLine);
+        }
+
+        public void AdminsCanDoThis()
+        {
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("Only the person with 'admin' role can invoke this method.");
+            Console.WriteLine(Environment.NewLine);
+        }
+    }
+}
---
+++ Auth/DemoApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var app = new MainApp();
+
+            app.AllCanDoThis();
+            app.AdminsCanDoThis();
+
+            Console.ReadLine();
+        }
+    }
+}
