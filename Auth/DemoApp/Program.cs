ccce9553f3399deed6a469f31073e9d7ba260def
Andrii Tkach <at@winkas.dk>
2014-11-28 15:41:08 +0200

    Invoking simple auth method

--- Auth/DemoApp/Program.cs
+++ Auth/DemoApp/Program.cs
@@ -6,10 +6,15 @@ using System.Threading.Tasks;
 
 namespace DemoApp
 {
+    using Auth;
+
     class Program
     {
         static void Main(string[] args)
         {
+            var auth = new Auth.Authenticator(new StubService());
+            auth.Authenticate("User1", "p1");
+
             var app = new MainApp();
 
             app.ShowInfo();
